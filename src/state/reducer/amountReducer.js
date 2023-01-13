9ccb9e6663ea6c9d2210c21d8d91dc7320817cd2
jamilahmed2 <ja093599@gmail.com>
2023-01-13 16:50:24 +0500

    redux store

--- src/state/reducer/amountReducer.js
+++ src/state/reducer/amountReducer.js
@@ -1,11 +1,12 @@
-export default reducer =(state=0,action)=>{
-    if (action.type=='deposit'){
+const reducer =(state=0,action)=>{
+    if (action.type ==='deposit'){
         return state + action.payload
     }
-    else if (action.type=='withdraw'){
+    else if (action.type ==='withdraw'){
         return state - action.payload
     }
     else{
         return state 
     }
 }
+export default reducer
---
+++ src/state/reducer/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import amountReducer from "./amountReducer";
+
+const  reducer = combineReducers({
+    amount:amountReducer
+})
+export default  reducer
